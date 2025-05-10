6a78354ad85a1b0bccd03ea715f063ae40b56dad
himbra <brahimanougmar6@gmail.com>
2025-05-10 16:07:03 +0000

    enabling redis

--- ecommerce-backend/src/main/java/dev/himbra/ecommercebackend/EcommerceBackendApplication.java
+++ ecommerce-backend/src/main/java/dev/himbra/ecommercebackend/EcommerceBackendApplication.java
@@ -2,10 +2,12 @@ package dev.himbra.ecommercebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableCaching
 public class EcommerceBackendApplication {
 
     public static void main(String[] args) {
